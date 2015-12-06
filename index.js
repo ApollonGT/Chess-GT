a2567dd83fc61f32196efbcb36dc6daf8a6663c3
Gkratsia Tantilian <gkratsia.tantilian@gmail.com>
2015-12-06 20:54:56 +0200

    Initial commit of Chess-GT UI and some basic movements Made with nodeJS-expressJS-Jade/AngularJS/Bootstrap/jQuery

---
+++ index.js
@@ -0,0 +1,115 @@
+'use strict';
+
+var appName = 'Chess GT';
+var appVersion = 'v1.0.0';
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.set('view engine', 'jade');
+app.set('views', 'views');
+app.set('json spaces', 2);
+app.locals.pretty = true;
+app.use("/scripts", express.static( __dirname + '/node_modules/'));
+app.use("/media", express.static( __dirname + '/public/img/'));
+app.use(express.static(__dirname + '/public/'));
+app.use(bodyParser.json());
+
+/* Database Connection */
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+var url = 'mongodb://localhost:3717/chess';
+/* Database Connection */
+
+/*      Start Server      */
+var server_cb = function () {
+    var port = server.address().port;
+    console.log(appName + ' ' + appVersion + ' listening at http://localhost:%s', port);
+}
+var server = app.listen(3030, server_cb);
+/*      Start Server      */
+
+/* Jade root view */
+var root_rt = function (req, res) {
+    res.render('index', {
+        title: appName,
+        version: appVersion
+    });
+}
+
+/* game api - json response */
+/*  LOAD */
+var load_game = function (req, res) {
+    MongoClient.connect(url, function(error, db) {
+        var parameters = req.body;
+        if (!error) {
+            var games = db.collection('chess_game');
+            games.find({"name": parameters.name}).toArray(function(error, docs) {
+                res.json(docs);
+                db.close();
+            });
+        }
+    });
+}
+
+/* SAVE */
+var saveGame = function (db, game, callback) {
+    var games = db.collection('chess_game');
+    games.insertOne(game, function(error, result) {
+        if (error) {
+            console.log(err(error));
+        } else if (callback) {
+            callback(result);
+        }
+    });
+}
+
+var save_game = function (req, res) {
+    MongoClient.connect(url, function(error, db) {
+        var parameters = req.body;
+        var game = {
+        };
+
+        saveGame(db, game, function() {
+            db.close();
+        });
+    });
+}
+
+/* DELETE */
+var deleteGame = function (db, game_name, callback) {
+    var games = db.collection('chess_game');
+
+    games.deleteOne({ "name": game_name }, function (error, result) {
+        if (error) {
+            console.log(err(error));
+        }
+
+        callback();
+    });
+}
+
+var delete_game = function (req, res) {
+    MongoClient.connect(url, function(error, db) {
+        var parameters = req.body;
+
+        deleteGame(db, parameters.name, function() {
+            db.close();
+        });
+    });
+}
+
+/* 404 */
+var notfound_rt = function (req, res) {
+    res.send("404 - page not found");
+}
+
+/* Jade root view */
+app.get('/', root_rt);
+app.get('/notfound', notfound_rt);
+app.post('/load', load_game);
+app.post('/save', save_game);
+app.post('/delete', delete_game);
+
+app.get('*', function(req, res) { res.redirect('/notfound'); });
---
+++ public/js/ctrl/CtrlMain.js
@@ -0,0 +1,145 @@
+(function(){
+    var app = angular.module("chess");
+    var move = 1;
+
+    function are_the_same(s1, s2)
+    {
+        return (s1.attr("data-row") == s2.attr("data-row") && s1.attr("data-column") == s2.attr("data-column"));
+    }
+
+    function are_opponents(s1, s2)
+    {
+        return s1.attr("id").charAt(0) != s2.attr("id").charAt(0);
+    }
+
+    function logMove(src, trgt)
+    {
+        var from = src.attr("data-column")+src.attr("data-row");
+        var to = trgt.attr("data-column")+trgt.attr("data-row");
+
+        $("#moves").prepend(move+". "+from+" - "+to+"<br/>");
+        move++;
+    }
+
+    function moveItem(id, tr, tc)
+    {
+        $("#"+id).appendTo(".step[data-row='"+tr+"'][data-column='"+tc+"']");
+    }
+
+    var ctrlMain = function (s, web) {
+        s.emptyBoard = function ()
+        {
+            move = 1;
+            $("#moves").html("");
+
+            $(".chess-set-item").appendTo("#chess-set-box");
+        }
+
+        s.saveBoard = function () {
+
+        }
+
+        s.resetBoard = function ()
+        {
+            move = 1;
+            $("#moves").html("");
+
+            moveItem("bp1", 7, "a");
+            moveItem("bp2", 7, "b");
+            moveItem("bp3", 7, "c");
+            moveItem("bp4", 7, "d");
+            moveItem("bp5", 7, "e");
+            moveItem("bp6", 7, "f");
+            moveItem("bp7", 7, "g");
+            moveItem("bp8", 7, "h");
+
+            moveItem("br1", 8, "a");
+            moveItem("br2", 8, "h");
+
+            moveItem("bb1", 8, "c");
+            moveItem("bb2", 8, "f");
+
+            moveItem("bn1", 8, "b");
+            moveItem("bn2", 8, "g");
+
+            moveItem("bk", 8, "e");
+            moveItem("bq", 8, "d");
+
+            moveItem("wp1", 2, "a");
+            moveItem("wp2", 2, "b");
+            moveItem("wp3", 2, "c");
+            moveItem("wp4", 2, "d");
+            moveItem("wp5", 2, "e");
+            moveItem("wp6", 2, "f");
+            moveItem("wp7", 2, "g");
+            moveItem("wp8", 2, "h");
+
+            moveItem("wr1", 1, "a");
+            moveItem("wr2", 1, "h");
+
+            moveItem("wb1", 1, "c");
+            moveItem("wb2", 1, "f");
+
+            moveItem("wn1", 1, "b");
+            moveItem("wn2", 1, "g");
+
+            moveItem("wk", 1, "e");
+            moveItem("wq", 1, "d");
+        }
+
+        s.stepMouseOver = function (e) {
+            var row = $(e.target).attr("data-row");
+            var column = $(e.target).attr("data-column");
+
+            $(".index[data-row='"+row+"']").addClass("highlight-index");
+            $(".index div[data-column='"+column+"']").addClass("highlight-index");
+        }
+
+        s.stepMouseLeave = function (e) {
+            var row = $(e.target).attr("data-row");
+            var column = $(e.target).attr("data-column");
+
+            $(".index[data-row='"+row+"']").removeClass("highlight-index");
+            $(".index div[data-column='"+column+"']").removeClass("highlight-index");
+        }
+
+        s.stepClick = function (e) {
+            var t = $(e.target);
+            if (!t.hasClass("step")) {
+                return;
+            }
+            if ($(".chess-set-item.active").length > 0 && !are_the_same($(".chess-set-item.active").parent(), t) && t.html().length > 0) {
+                logMove($(".chess-set-item.active").parent(), t);
+                t.find(".chess-set-item").appendTo("#chess-set-box");
+                moveItem($(".chess-set-item.active").attr("id"), t.attr("data-row"), t.attr("data-column"));
+                $(".chess-set-item.active").removeClass("active");
+            } else if ($(".chess-set-item.active").length > 0 && !are_the_same($(".chess-set-item.active").parent(), t)) {
+                logMove($(".chess-set-item.active").parent(), t);
+                moveItem($(".chess-set-item.active").attr("id"), t.attr("data-row"), t.attr("data-column"));
+                $(".chess-set-item.active").removeClass("active");
+            }
+        }
+
+        s.itemClick = function (e) {
+            var t = $(e.target);
+
+            if (t.hasClass("active")) {
+                t.removeClass("active");
+            } else if ($(".chess-set-item.active").length > 0 && are_opponents(t, $(".chess-set-item.active"))) {
+                logMove($(".chess-set-item.active").parent(), t.parent());
+                moveItem($(".chess-set-item.active").attr("id"), t.parent().attr("data-row"), t.parent().attr("data-column"));
+                $(".chess-set-item.active").removeClass("active");
+                t.appendTo("#chess-set-box");
+            } else {
+                $(".chess-set-item.active").removeClass("active");
+                t.addClass("active");
+            }
+        }
+
+        angular.element(document).ready(function(){
+            s.resetBoard();
+        })
+    }
+
+    app.controller("ctrlMain", ["$scope", "$http", ctrlMain]);
+}());
